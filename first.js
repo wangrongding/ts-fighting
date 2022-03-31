1a40659f6ec33f9427dce333eb2471b83c71a891
wangrongding <wangrongding@qq.com>
2022-04-01 00:04:56 +0800

    chore: init

---
+++ first.js
@@ -0,0 +1,42 @@
+// class Person {
+//   job: string;
+//   // name: string = "123";
+//   //私有属性
+//   private age: number;
+//   //公有属性
+//   public say() {
+//     console.log(this.name);
+//     console.log(this.toll);
+//   }
+//   //受保护的属性
+//   protected sayAge() {
+//     console.log("protected");
+//   }
+//   private say3() {
+//     console.log("private");
+//   }
+//   //构造函数
+//   constructor(public name: string, public toll?: string) {
+//     this.name = name;
+//   }
+//   //静态属性
+//   static say2() {
+//     console.log("static");
+//     // return 123;
+//   }
+// }
+// new Person("123", "1").say();
+// Person.say2();
+// class Man {
+//   //静态属性
+//   static say() {
+//     console.log("static");
+//     // return 123;
+//   }
+// }
+// Man.say();
+var tempArr = function (tempArr) {
+    return "123";
+};
+tempArr([123]);
+// const tempArr = (tempArr: <T>) => {};
